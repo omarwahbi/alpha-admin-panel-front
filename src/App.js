0acbbe10919e63cb04e681f04c9c1a0b8dc53a7b
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-14 02:13:21 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Project from "./pages/Project";
 import Logos from "./pages/Logos";
 import Categories from "./pages/Categories";
 import ContactUs from "./pages/ContactUs";
-axios.defaults.withCredentials = true;
+import api from "./Components/Api";
+api.defaults.withCredentials = true;
 const router = createBrowserRouter([
   {
     path: "/",
