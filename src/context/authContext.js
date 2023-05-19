76a431fd901c639fa0f7cc21033782a7ce523800
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-19 18:33:37 +0300

    fixes

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,21 +1,23 @@
-// AuthContext.js
-
-import { createContext, useState, useEffect } from "react";
+import Api from "../Components/Api.js";
+import React, { createContext, useEffect, useState } from "react";
 import Cookies from "universal-cookie";
-import Api from "../Components/Api";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(); // Create a context using createContext()
+const cookies = new Cookies();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
     JSON.parse(localStorage.getItem("user") || null)
   );
 
-  const cookies = new Cookies();
-  const accessToken = cookies.get("access_token");
-
   const login = async (input) => {
     const res = await Api.post("/auth/login", input);
+    cookies.set("access_token", res.data, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      domain: ".vercel.app",
+    });
     setCurrentUser(res.data);
   };
 
@@ -26,11 +28,11 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
-  }, [currentUser]);
+    localStorage.setItem("user", JSON.stringify(currentUser)); // Provide a key for storing the value in localStorage
+  }, [currentUser]); // Add currentUser as a dependency in the dependency array
 
   return (
-    <AuthContext.Provider value={{ currentUser, login, logout, accessToken }}>
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
